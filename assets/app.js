4e391f0e05d00beddeaf1029e1352d39cf2ead64
sharky <zeebing@gmail.com>
2019-04-03 13:04:31 -0400

    made question constructor

--- assets/app.js
+++ assets/app.js
@@ -1,1 +1,15 @@
 //question constructor
+function Question(question,a1,a2,a3,a4,answerID){
+    this.question = "",
+    this.a1 = "",
+    this.a2 = "",
+    this.a3 = "",
+    this.a4 = "",
+    this.answerID = ""
+
+};
+
+var questions = [];
+
+questions[0] = new Question();
+
