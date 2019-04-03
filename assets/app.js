380749bb8739e38f8cfa846575f56550eca0a50b
sharky <zeebing@gmail.com>
2019-04-03 14:32:36 -0400

    created question constructor and function to load answers into page

--- assets/app.js
+++ assets/app.js
@@ -1,15 +1,28 @@
 //question constructor
-function Question(question,a1,a2,a3,a4,answerID){
-    this.question = "",
-    this.a1 = "",
-    this.a2 = "",
-    this.a3 = "",
-    this.a4 = "",
-    this.answerID = ""
+function Question(question, answers, answerID){
+    this.question = question,
+    this.answers = answers,
+    this.answerID = answerID
 
 };
-
+//array of questions
 var questions = [];
+//set questions
+questions[0] = new Question(
+    "How many flowers must honey bees visit to make one pound of honey?", 
+    [{id: "a1", text:"2,000,000"},
+    {id: "a2", text:"1,000,000"},
+    {id: "a3", text:"500,000"},
+    {id: "a4", text: "1 but a lot of times",}
+],"a1");
+loadAnswers(questions[0]);
+//
+function loadAnswers(questionObject){
+    var question = questionObject;
 
-questions[0] = new Question();
+    for(var i = 0; i < question.answers.length; i++){
+        var answer = question.answers[i];
+        $("#"+answer.id).text(answer.text);
+    }
 
+}
