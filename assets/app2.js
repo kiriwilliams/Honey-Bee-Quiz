6e97ef74084a96f9d87924a8b86a2ca29aa94633
sharky <zeebing@gmail.com>
2019-04-04 10:57:25 -0400

    created new function nextMove to check if there are remaining questions and display the next question, else call gameOver function. Also fixed timer for unanswered questions. Also added function makeDiv to append the number of correct/incorrect/unanswered to the game div.

--- assets/app2.js
+++ assets/app2.js
@@ -1,8 +1,8 @@
 //question constructor
-function Question(question, answers, answerID){
+function Question(question, answers, answerID) {
     this.question = question,
-    this.answers = answers,
-    this.answerID = answerID
+        this.answers = answers,
+        this.answerID = answerID
 
 };
 //array of questions
@@ -12,182 +12,210 @@ var current;
 var correct = 0;
 var incorrect = 0;
 var unanswered = 0;
-var time = 10;
+var time = 5;
 var timer;
 
 //set questions
-questions[0] = new Question("How many flowers must honey bees visit to make one pound of honey?",[
-    {id: "a1", text:"2,000,000"},
-    {id: "a2", text:"1,000,000"},
-    {id: "a3", text:"500,000"},
-    {id: "a4", text: "1 but a lot of times"}
-],"a1");
-questions[1] = new Question("How far can a honey bee fly?",[
-    {id: "a1", text: "1 mile"},
-    {id: "a2", text: "1/2 mile"},
-    {id: "a3", text: "1.5 miles"},
-    {id: "a4", text: "6 miles"}
-], "a4");
-questions[2] = new Question("How much honey can a single honey bee produce in its lifetime?",[
-    {id: "a1", text: "1/12 teaspoon"},
-    {id: "a2", text: "1/8 teaspoon"},
-    {id: "a3", text: "1/2 teaspoon"},
-    {id: "a4", text: "1/2 tablespoon"}
-], "a4");
-questions[3] = new Question("How do honey bees communicate with each other?",[
-    {id: "a1", text: "Smell"},
-    {id: "a2", text: "Dance"},
-    {id: "a3", text: "Sound"},
-    {id: "a4", text: "All of the above"}
-], "a4");
-questions[4] = new Question("How long can a queen live?",[
-    {id: "a1", text: "3 years"},
-    {id: "a2", text: "5 years"},
-    {id: "a3", text: "9 years"},
-    {id: "a4", text: "12 years"}
-], "a2");
-questions[5] = new Question("What do drone honey bees do?",[
-    {id: "a1", text: "Protect the hive"},
-    {id: "a2", text: "Mate with the queen"},
-    {id: "a3", text: "Take care of the larvae"},
-    {id: "a4", text: "Clean the hive"}
-], "a2");
-questions[6] = new Question("How does a bee become queen?",[
-    {id: "a1", text: "A larva is fed royal jelly"},
-    {id: "a2", text: "A worker eats the previous queen"},
-    {id: "a3", text: "A drone will pick a new queen"},
-    {id: "a4", text: "There's an election (first past the post)"}
+questions[0] = new Question("How many flowers must honey bees visit to make one pound of honey?", [
+    { id: "a1", text: "2,000,000" },
+    { id: "a2", text: "1,000,000" },
+    { id: "a3", text: "500,000" },
+    { id: "a4", text: "1 but a lot of times" }
 ], "a1");
-questions[7] = new Question("What do drones do in the winter?",[
-    {id: "a1", text: "They hibernate in the hive"},
-    {id: "a2", text: "They get chased out of the hive"},
-    {id: "a3", text: "They take on new duties while the workers hibernate"},
-    {id: "a4", text: "They offer their bodies as sustenance to the queen"}
+questions[1] = new Question("How far can a honey bee fly?", [
+    { id: "a1", text: "1 mile" },
+    { id: "a2", text: "1/2 mile" },
+    { id: "a3", text: "1.5 miles" },
+    { id: "a4", text: "6 miles" }
+], "a4");
+questions[2] = new Question("How much honey can a single honey bee produce in its lifetime?", [
+    { id: "a1", text: "1/12 teaspoon" },
+    { id: "a2", text: "1/8 teaspoon" },
+    { id: "a3", text: "1/2 teaspoon" },
+    { id: "a4", text: "1/2 tablespoon" }
+], "a4");
+questions[3] = new Question("How do honey bees communicate with each other?", [
+    { id: "a1", text: "Smell" },
+    { id: "a2", text: "Dance" },
+    { id: "a3", text: "Sound" },
+    { id: "a4", text: "All of the above" }
+], "a4");
+questions[4] = new Question("How long can a queen live?", [
+    { id: "a1", text: "3 years" },
+    { id: "a2", text: "5 years" },
+    { id: "a3", text: "9 years" },
+    { id: "a4", text: "12 years" }
 ], "a2");
-questions[8] = new Question("How fast can a honey bee fly?",[
-    {id: "a1", text: "5 mph"},
-    {id: "a2", text: "10 mph"},
-    {id: "a3", text: "15 mph"},
-    {id: "a4", text: "20 mph"}
+questions[5] = new Question("What do drone honey bees do?", [
+    { id: "a1", text: "Protect the hive" },
+    { id: "a2", text: "Mate with the queen" },
+    { id: "a3", text: "Take care of the larvae" },
+    { id: "a4", text: "Clean the hive" }
+], "a2");
+questions[6] = new Question("How does a bee become queen?", [
+    { id: "a1", text: "A larva is fed royal jelly" },
+    { id: "a2", text: "A worker eats the previous queen" },
+    { id: "a3", text: "A drone will pick a new queen" },
+    { id: "a4", text: "There's an election (first past the post)" }
+], "a1");
+questions[7] = new Question("What do drones do in the winter?", [
+    { id: "a1", text: "They hibernate in the hive" },
+    { id: "a2", text: "They get chased out of the hive" },
+    { id: "a3", text: "They take on new duties while the workers hibernate" },
+    { id: "a4", text: "They offer their bodies as sustenance to the queen" }
+], "a2");
+questions[8] = new Question("How fast can a honey bee fly?", [
+    { id: "a1", text: "5 mph" },
+    { id: "a2", text: "10 mph" },
+    { id: "a3", text: "15 mph" },
+    { id: "a4", text: "20 mph" }
 ], "a3");
-questions[9] = new Question("How many eggs can a queen lay per day in the summer?",[
-    {id: "a1", text: "2500"},
-    {id: "a2", text: "25000"},
-    {id: "a3", text: "5000"},
-    {id: "a4", text: "50000"}
+questions[9] = new Question("How many eggs can a queen lay per day in the summer?", [
+    { id: "a1", text: "2500" },
+    { id: "a2", text: "25000" },
+    { id: "a3", text: "5000" },
+    { id: "a4", text: "50000" }
 ], "a1");
-questions[10] = new Question("Why do bees buzz?",[
-    {id: "a1", text: "Angry bees buzz as a warning"},
-    {id: "a2", text: "They use it for echolocation"},
-    {id: "a3", text: "They can't help it"},
-    {id: "a4", text: "A swarm of bees buzz together as an affirmation of kinship"}
+questions[10] = new Question("Why do bees buzz?", [
+    { id: "a1", text: "Angry bees buzz as a warning" },
+    { id: "a2", text: "They use it for echolocation" },
+    { id: "a3", text: "They can't help it" },
+    { id: "a4", text: "A swarm of bees buzz together as an affirmation of kinship" }
 ], "a3");
 
+$(document).ready(function () {
 
-$("#start").on("click", function(){
-    reset();
+    $("#start").on("click", function () {
+        reset();
+    });
+
+    function reset() {
+        next = 0;
+        correct = 0;
+        incorrect = 0;
+        unanswered = 0;
+        time = 5;
+        showQuestion(next);
+    }
+
+
+    //takes an index number, shows the current question
+    function showQuestion(i) {
+        resetTimer();
+        startTimer();//start the timer
+        current = questions[i]; //update the current question (saving this object for global use)
+        $("#game").empty();
+        $("#game").append(
+            "<div class='row'><div id='question' class='col-md-12'>" + current.question + "</div></div>");
+
+        loadAnswers(current); //load answers for the current question
+        next++; //increment the next question (using as a counter)
+        allowClicks();
+
+    }
+    //takes a questionObject, displays all possible answers as buttons
+    function loadAnswers(questionObject) {
+        var question = questionObject;
+        //loop through items in the answers array
+        for (var i = 0; i < question.answers.length; i++) {
+            var answer = question.answers[i]; //save the current answer
+
+            $("#game").append("<div class='row'><button id='" + answer.id + "' class='btn btn-outline-primary col-md-12'>" + answer.text + "</button></div>");
+        }
+    }
+    //add click listener to all buttons
+    function allowClicks() {
+        $(".btn").on("click", function () {
+            stopTimer();
+            //don't let user click other buttons after making a choice
+            freezeClicks();
+            checkAnswer(this);
+            console.log("click");
+
+        });
+    }
+    //remove click listener from buttons
+    function freezeClicks() {
+        $(".btn").off("click");
+    }
+
+
+    function nextMove() {
+        stopTimer();
+        if (next < questions.length) {
+            setTimeout(function () {
+                showQuestion(next);
+            }, 1500);
+        }
+        else {//game is over
+           setTimeout(function () {
+                gameOver();
+            }, 1500);
+        }
+    }
+    //takes a button element, checks its ID against the correct answerID
+    function checkAnswer(button) {
+        var button = $(button);
+
+        //if the clicked answer is the same as the correct answer
+        if (button.attr("id") == current.answerID) {
+            //show correct answer in green
+            button.removeClass("btn-outline-primary").addClass("btn-success");
+            correct++;
+            say("Good Job!");
+        }
+        else {
+            //show this answer as incorrect
+            button.removeClass("btn-outline-primary").addClass("btn-danger");
+            //show the correct answer in green
+            $("#" + current.answerID).removeClass("btn-outline-primary").addClass("btn-success");
+            incorrect++;
+            say("Not quite...");
+        }
+        nextMove();
+    }
+
+    function say(message) {
+
+    }
+    function gameOver() {
+        console.log("game over");
+        $("#game").empty();
+        $("#game").append(makeDiv("Correct: "+correct)).append(makeDiv("Incorrect: "+incorrect)).append(makeDiv("Unanswered: "+unanswered));
+
+        // $("#correct").text(correct);
+        // $("#incorrect").text(incorrect);
+        // $("#unanswered").text(unanswered);
+    }
+    function countdown() {
+        time--;
+        $("#time").text(time);
+        if (time < 1) {
+            unanswered++;
+
+            // say("Time's up!");
+            nextMove();
+        }
+    }
+
+    //start the timer
+    function startTimer() {
+        timer = setInterval(countdown, 1000);
+    }
+
+    //stop the timer
+    function stopTimer() {
+        clearInterval(timer);
+    }
+
+    //reset the timer
+    function resetTimer() {
+        time = 5;
+        $("#time").text(time);
+    }
+
+    function makeDiv(content){
+        return "<div>"+content+"</div>"
+    }
+
 });
-  
-
-function reset(){
-    next = 0;
-    correct = 0;
-    incorrect = 0;
-    unanswered = 0;
-    time = 10;
-    showQuestion(next);
-}
-if(time < 1){
-    unanswered++;
-    resetTimer();
-    showQuestion(next);
-}
-
-//takes an index number, shows the current question
-function showQuestion(i){
-    resetTimer();
-    startTimer();//start the timer
-    current = questions[i]; //update the current question (saving this object for global use)
-    $("#game").empty();
-    $("#game").append(
-        "<div class='row'><div id='question' class='col-md-12'>"+current.question+"</div></div>");
-
-    loadAnswers(current); //load answers for the current question
-    next++; //increment the next question (using as a counter)
-    allowClicks();
-
-}
-//takes a questionObject, displays all possible answers as buttons
-function loadAnswers(questionObject){
-    var question = questionObject;
-    //loop through items in the answers array
-    for(var i = 0; i < question.answers.length; i++){
-        var answer = question.answers[i]; //save the current answer
-
-        $("#game").append("<div class='row'><button id='"+answer.id+"' class='btn btn-outline-primary col-md-12'>"+answer.text+"</button></div>");
-    }
-}
-//add click listener to all buttons
-function allowClicks(){
-$(".btn").on("click",function(){
-    stopTimer();
-    //don't let user click other buttons after making a choice
-    freezeClicks();
-    checkAnswer(this);
-    console.log("click");
-
-});  
-}
-//remove click listener from buttons
-function freezeClicks(){
-    $(".btn").off("click");
-}
-//takes a button element, checks its ID against the correct answerID
-function checkAnswer(button){
-    var button = $(button);
-
-    //if the clicked answer is the same as the correct answer
-    if (button.attr("id") == current.answerID){
-        //show correct answer in green
-        button.removeClass("btn-outline-primary").addClass("btn-success");
-        correct++;
-        say("Good Job!");
-    }
-    else{
-        //show this answer as incorrect
-        button.removeClass("btn-outline-primary").addClass("btn-danger");
-        //show the correct answer in green
-        $("#"+current.answerID).removeClass("btn-outline-primary").addClass("btn-success");
-        incorrect++;
-        say("Not quite...");
-    }
-    if(next < questions.length){
-        setTimeout(function(){
-            showQuestion(next);
-        },1500);
-    }
-    else{//game is over
-       gameOver();
-    }
-}
-
-function say(message){
-
-}
-function gameOver(){
-
-}
-function countdown(){
-    time--;
-    $("#time").text(time);
-}
-function startTimer(){
-    timer = setInterval(countdown,1000);
-}
-function stopTimer(){
-    clearInterval(timer);
-}
-function resetTimer(){
-    time = 10;
-    $("#time").text(time);
-}
